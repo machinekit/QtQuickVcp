c5bc74516fc702112837ed18fdc22b51379094ac
Alexander Rössler <mail.aroessler@gmail.com>
2014-04-18 19:13:56 +0200

    rm submodule

--- setup-project.sh
+++ setup-project.sh
@@ -1,15 +1,6 @@
 #!/bin/bash
-git submodule init
-git submodule update
 
 #Setup nzmqt
 cd ./3rdparty/nzmqt/
 ./setup-project.sh
 cd ../../
-
-# Setup project directories.
-#mkdir -p externals/include
-
-# Copy files.
-#cp externals-src/cppzmq/zmq.hpp externals/include
-
