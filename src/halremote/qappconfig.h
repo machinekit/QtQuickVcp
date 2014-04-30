b18faa88763935bfc8b8eacadc8b13dd610a5d1a
Alexander Rössler <mail.aroessler@gmail.com>
2014-04-30 16:38:31 +0200

    Added license headers

--- src/halremote/qappconfig.h
+++ src/halremote/qappconfig.h
@@ -1,3 +1,24 @@
+/****************************************************************************
+**
+** Copyright (C) 2014 Alexander Rössler
+** License: LGPL version 2.1
+**
+** This file is part of QtQuickVcp.
+**
+** All rights reserved. This program and the accompanying materials
+** are made available under the terms of the GNU Lesser General Public License
+** (LGPL) version 2.1 which accompanies this distribution, and is available at
+** http://www.gnu.org/licenses/lgpl-2.1.html
+**
+** This library is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+** Lesser General Public License for more details.
+**
+** Contributors:
+** Alexander Rössler @ The Cool Tool GmbH <mail DOT aroessler AT gmail DOT com>
+**
+****************************************************************************/
 #ifndef QAPPCONFIG_H
 #define QAPPCONFIG_H
 
