a3b9adb05c324e917aa0591ce40c6bbf52777f8e
Alexander Rössler <mail.aroessler@gmail.com>
2016-10-07 20:52:34 +0200

    build: testing download

--- build/appveyor/appveyor.bat
+++ build/appveyor/appveyor.bat
@@ -1,5 +1,7 @@
 setlocal
 
+appveyor DownloadFile http://buildbot.roessler.systems/files/qt-bin/Qt-5.7-Android-armv7.tar.bz2 -Filename qt5.zip
+
 cd %APPVEYOR_BUILD_FOLDER%
 mkdir build.release
 cd build.release
