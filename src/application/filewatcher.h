d17a59022d6b57bca46f2173cca50507bdde19ba
Alexander Rössler <mail@roessler.systems>
2017-11-04 19:35:34 +0100

    FileWatcher: add recursion support

--- src/application/filewatcher.h
+++ src/application/filewatcher.h
@@ -12,30 +12,37 @@ class FileWatcher : public QObject
     Q_OBJECT
     Q_PROPERTY(QUrl fileUrl READ fileUrl WRITE setFileUrl NOTIFY fileUrlChanged)
     Q_PROPERTY(bool enabled READ enabled WRITE setEnabled NOTIFY enabledChanged)
+    Q_PROPERTY(bool recursive READ recursive WRITE setRecursive NOTIFY recursiveChanged)
 
 public:
     explicit FileWatcher(QObject *parent = nullptr);
 
     QUrl fileUrl() const;
     bool enabled() const;
+    bool recursive() const;
 
 signals:
     void fileChanged();
     void fileUrlChanged(const QUrl &fileUrl);
     void enabledChanged(bool enabled);
 
+    void recursiveChanged(bool recursive);
+
 public slots:
     void setFileUrl(const QUrl &fileUrl);
     void setEnabled(bool enabled);
+    void setRecursive(bool recursive);
 
 private:
     QUrl m_fileUrl;
     bool m_enabled;
+    bool m_recursive;
     QFileSystemWatcher m_fileSystemWatcher;
 
 private slots:
     void updateWatchedFile();
     void onWatchedFileChanged();
+    void onWatchedDirectoryChanged(const QString &path);
 
 }; // class FileWatcher
 } // namespace qtquickvcp
