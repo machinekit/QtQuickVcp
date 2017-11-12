09528fcdee3c115506d129ae13047ea876e23fa1
Alexander Rössler <mail@roessler.systems>
2017-11-12 12:47:28 +0100

    MachinetalkService: remove old implementations

--- src/machinetalk/machinetalkservice.h
+++ src/machinetalk/machinetalkservice.h
@@ -21,22 +21,12 @@ namespace qtquickvcp {
 namespace MachinetalkService
 {
     QString enumNameToCamelCase(const QString &name);
-    void recurseDescriptor(const google::protobuf::Descriptor *descriptor, QJsonObject &object);
-    QObject* recurseDescriptor(const google::protobuf::Descriptor *descriptor, QQmlEngine *engine, QObject *parent);
-    int recurseMessage(const google::protobuf::Message &message, QJsonObject &object,
-                       const QString &fieldFilter = QString(), const QString &tempDir = QString("json"));
-    int recurseMessage(const google::protobuf::Message &message, QQmlEngine *engine, QObject *object,
-                       const QString &tempDir = QString("json"), const QString &fieldFilter = QString());
     QObject* recurseDescriptor(const google::protobuf::Descriptor *descriptor, QObject *parent,
                                const QString &fieldFilter = QString());
     int recurseMessage(const google::protobuf::Message &message, QObject *object,
                        const QString &tempDir = QString("json"), const QString &fieldFilter = QString());
-    void updateValue(const google::protobuf::Message &message, QJsonValue &value,
-                     const QString &field, const QString &tempDir = QString("json"));
 
     void updateSimpleRepeatedField(QObject *object, const google::protobuf::Message &message, const google::protobuf::FieldDescriptor *field);
-    void updateComplexRepeatedField(QObject *object, const google::protobuf::Message &message, const google::protobuf::FieldDescriptor *field,
-                                    QQmlEngine *engine, const QString &tempDir);
     void updateComplexRepeatedField(QObject *object, const google::protobuf::Message &message, const google::protobuf::FieldDescriptor *field,
                                     const QString &tempDir);
     QVariant simpleFieldValueToVariant(const google::protobuf::Message &message, const google::protobuf::FieldDescriptor *field);
@@ -44,7 +34,6 @@ namespace MachinetalkService
     /** Converts a protobuf File object to a json file descriptor
      *  stores the data to a temporary directory
      **/
-    void fileToJson(const machinetalk::File &file, QJsonObject &object, const QString &tempDir);
     void fileToObject(const machinetalk::File &file, QObject *object, const QString &tempDir);
 
     QByteArray recurseDescriptor(const google::protobuf::Descriptor *descriptor, QByteArray &qmlCode);
